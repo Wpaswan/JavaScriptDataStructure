225bcc346df9b7c93932ba1dfeebedff1e765826
Wpaswan <dablumcapaswan@gmail.com>
2022-01-12 08:28:22 -0800

    [I added a code to find the triplet]

---
+++ ArrayPracticeProble/FindTriplet.js
@@ -0,0 +1,29 @@
+arr = [0, -1, 2, -3, 1];
+    
+// Prints all triplets in arr[] with 0 sum    
+    
+        
+        for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            for (let k = j + 1; k < arr.length; k++) {
+                if (arr[i] + arr[j] + arr[k] === 0)
+                {
+                    console.log(arr[i]);
+                    console.log(" ");
+                   console.log(arr[j]);
+                    console.log(" ");
+                    console.log(arr[k]);
+                    console.log()
+                    found = true;
+                   
+                }
+            }
+        }
+        // If no triplet with 0 sum found in array
+        if(found === false) {
+            document.write(" not exist ");
+        }
+            else{
+            console.log("Found")
+            }
+        }
